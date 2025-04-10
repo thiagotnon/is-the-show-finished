6ae59058dfb4759b73bbb2a610e763374566594a
Thiago Natan <36268854+thiagotnon@users.noreply.github.com>
2025-04-10 17:49:53 -0300

    add adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,12 @@ export default function RootLayout({
           src='https://www.googletagmanager.com/gtag/js?id=G-T116Z2TCLP'
         />
         <Script id='google_analytics' strategy='lazyOnload' src='/g_analytics.js' />
+
+        <Script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7783316904653454'
+          crossOrigin='anonymous'
+        />
       </body>
     </html>
   );
